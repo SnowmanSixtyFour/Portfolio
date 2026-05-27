22a219dd91e0436282419949583fcb9a454d6ac7
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-05-27 15:38:56 -0400

    Reworking Index Page
    Updated Favicon, CSS Styles, Index Page, and Objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // --- Variables ---
+
+  // Strings
+
+  let header =
+  `   CCC  H   H RRRR  III  SSSS       RRRR  Y   Y  CCC  ZZZZZ K   K EEEEE
+  C   C H   H R   R  I  S           R   R  Y Y  C   C    Z  K  K  E    
+  C     HHHHH RRRR   I   SSS        RRRR    Y   C       Z   KKK   EEEE 
+  C   C H   H R  R   I      S       R  R    Y   C   C  Z    K  K  E    
+   CCC  H   H R   R III SSSS        R   R   Y    CCC  ZZZZZ K   K EEEEE`;
+
   return (
-    <h1>Chris Ryczke</h1>
+    <div>
+      <p className="separator"/>
+      {header}
+      <p className="separator"/>
+    </div>
   )
 }
 
