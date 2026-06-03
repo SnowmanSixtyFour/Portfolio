a9e1d075576cb539853c5ead5723904728890575
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-06-03 14:49:11 -0400

    Updates

--- components/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function NavBar() {
-  return (
-    <div><a className="navButton" href="./">Home</a><a className="navButton" href="./projects">Projects</a><a className="navButton" href="./socials">Social Media</a><a className="navButton" href="./contacts">Contacts</a></div>
-  )
-}
-
-export default NavBar
