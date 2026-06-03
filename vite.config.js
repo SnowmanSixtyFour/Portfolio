b51c27016001b1eb057d8c7e9df902dc315a002b
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-06-03 14:59:38 -0400

    Updated Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  server: {
+    cors: {
+      origin: 'https://www.chrisryczke.com',
+    },
+  },
+  build: {
+    manifest: true,
+    rolldownOptions: {
+      input: '/src/main.jsx',
+      404: '/src/main.jsx', // Always go to index.html for any 404s, so that React Router can handle the routing
+    },
+  },
 })
