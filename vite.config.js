8fe9f7026c2aed0ce2622161d33044ccaf5ad937
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-06-03 15:06:04 -0400

    Added CNAME to public/

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
     manifest: true,
     rolldownOptions: {
       input: '/src/main.jsx',
-      404: 'index.html', // Always go to index.html for any 404s, so that React Router can handle the routing
     },
   },
 })
