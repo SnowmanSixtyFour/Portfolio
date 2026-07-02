18a9f7a11241929db586b2d458f28c6aba7699dc
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-02 00:02:27 -0400

    Fixed Resume Hyperlink

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import NavBar from '../NavBar';
+import Resume from "../../../src/assets/ChrisRyczkeResume.pdf";
 
 function Contacts() {
   const [count, setCount] = useState(0)
@@ -7,8 +8,8 @@ function Contacts() {
   return (
     <div>
       <p>Contacts</p>
-      <br></br>
-      <a className="navButton" href="https://www.linkedin.com/in/chris-ryczke/">LinkedIn</a> <a className="navButton" href="https://github.com/SnowmanSixtyFour">GitHub</a> <a className="navButton" href="src\assets\ChrisRyczkeResume.pdf">Resume</a> <a className="navButton" href="./socials">Social Media</a>
+      <br/>
+      <a className="navButton" href="https://www.linkedin.com/in/chris-ryczke/">LinkedIn</a> <a className="navButton" href="https://github.com/SnowmanSixtyFour">GitHub</a> <a className="navButton" href={Resume} target="_blank">Resume</a> <a className="navButton" href="./socials">Social Media</a>
       <p className="separator"/>
     </div>
   )
