d2bb09348d5a8d7fbb86681765529420250caf74
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-02 19:17:00 -0400

    Fixed Project Hyperlinks

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -54,7 +54,7 @@ function Projects() {
       <p>Projects</p>
         <h2>Blue Bird Series</h2>
           <h3>Blue Bird 64</h3>
-          <a href="https://snowman64.itch.io/bluebird64" target="_blank"> <img class="rounded" src="../../../src/assets/img/bluebird64.jpg" alt="Blue Bird 64"/></a>
+          <a href="https://snowman64.itch.io/bluebird64" target="_blank"> <img src="../../../src/assets/img/bluebird64.jpg" alt="Blue Bird 64"/></a>
           <p>Blue Bird 64 is an open-source game made in 2 days with Unity.</p>
           <p className="smaller">Windows, Mac and Linux builds are available on Game Jolt and itch.io.</p>
           <a href="https://github.com/SnowmanSixtyFour/BlueBird64" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
@@ -62,24 +62,24 @@ function Projects() {
           <a href="https://gamejolt.com/games/bluebird64/1065366" target="_blank"><img src="../../../src/assets/img/gamejolt_button.webp" width="40" height="40" alt="Game Jolt Page" title="Game Jolt Page"/></a>
           <a href="https://www.newgrounds.com/portal/view/1030173" target="_blank"><img src="../../../src/assets/img/newgrounds_button.webp" width="40" height="40" alt="Newgrounds Page" title="Newgrounds Page"/></a>
           <h3>Blue Bird 64 x2</h3>
-          <a href="https://snowman64.itch.io/bluebird64" target="_blank"> <img class="rounded" src="../../../src/assets/img/bluebird64x2.jpg" alt="Blue Bird 64 x2"/></a>
+          <a href="https://snowman64.itch.io/bluebird64-x2" target="_blank"> <img src="../../../src/assets/img/bluebird64x2.jpg" alt="Blue Bird 64 x2"/></a>
           <p>Blue Bird 64 x2 is an online Unity multiplayer game made in 7 days.</p>
-          <a href="https://github.com/SnowmanSixtyFour/BlueBird64" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
-          <a href="https://snowman64.itch.io/bluebird64" target="_blank"><img src="../../../src/assets/img/itchio_button.webp" width="40" height="40" alt="itch.io Page" title="itch.io Page"/></a>
-          <a href="https://gamejolt.com/games/bluebird64/1065366" target="_blank"><img src="../../../src/assets/img/gamejolt_button.webp" width="40" height="40" alt="Game Jolt Page" title="Game Jolt Page"/></a>
           <p className="smaller">Features lobbies up to 8 players big, and a quick play button.</p>
+          <a href="https://github.com/SnowmanSixtyFour/BlueBird64-x2" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
+          <a href="https://snowman64.itch.io/bluebird64-x2" target="_blank"><img src="../../../src/assets/img/itchio_button.webp" width="40" height="40" alt="itch.io Page" title="itch.io Page"/></a>
+          <a href="https://gamejolt.com/games/bluebird64-x2/1070507" target="_blank"><img src="../../../src/assets/img/gamejolt_button.webp" width="40" height="40" alt="Game Jolt Page" title="Game Jolt Page"/></a>
         <h2>Between the Lines</h2>
-        <a href="https://snowman64.itch.io/bluebird64" target="_blank"> <img class="rounded" src="../../../src/assets/img/betweenthelines.jpg" alt="Between the Lines"/></a>
+        <a href="https://snowman64.itch.io/between-the-lines" target="_blank"> <img src="../../../src/assets/img/betweenthelines.jpg" alt="Between the Lines"/></a>
         <p>Between the Lines is an indie game made for Mystery Game Jam 2026.</p>
         <p className="smaller">Originally developed from April 7, 2026 - May 5, 2026.</p>
-        <a href="https://github.com/SnowmanSixtyFour/BlueBird64" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
-        <a href="https://snowman64.itch.io/bluebird64" target="_blank"><img src="../../../src/assets/img/itchio_button.webp" width="40" height="40" alt="itch.io Page" title="itch.io Page"/></a>
-        <a href="https://gamejolt.com/games/bluebird64/1065366" target="_blank"><img src="../../../src/assets/img/gamejolt_button.webp" width="40" height="40" alt="Game Jolt Page" title="Game Jolt Page"/></a>
+        <a href="https://github.com/SnowmanSixtyFour/BetweenTheLines" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
+        <a href="https://snowman64.itch.io/between-the-lines" target="_blank"><img src="../../../src/assets/img/itchio_button.webp" width="40" height="40" alt="itch.io Page" title="itch.io Page"/></a>
+        <a href="https://gamejolt.com/games/between-the-lines/1066244" target="_blank"><img src="../../../src/assets/img/gamejolt_button.webp" width="40" height="40" alt="Game Jolt Page" title="Game Jolt Page"/></a>
         <h2>My Portfolio</h2>
-        <a href="https://snowman64.itch.io/bluebird64" target="_blank"> <img class="rounded" src="../../../src/assets/img/portfolio.jpg" alt="My Portfolio"/></a>
+        <a href="https://www.chrisryczke.com" target="_blank"> <img src="../../../src/assets/img/portfolio.jpg" alt="My Portfolio"/></a>
         <p>The portfolio you're browsing now was developed by me!</p>
         <p className="smaller">Not an ounce of the portfolio was made with a website designer. I have full control over the code.</p>
-        <a href="https://github.com/SnowmanSixtyFour/BlueBird64" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
+        <a href="https://github.com/SnowmanSixtyFour/Portfolio" target="_blank"><img src="../../../src/assets/img/github_button.webp" width="40" height="40" alt="GitHub Repository" title="GitHub Repository"/></a>
       <p className="separator"/>
     </div>
   )
