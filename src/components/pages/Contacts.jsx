a16c2b1e62d92508a84967ae2d273851ac37bbe7
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-02 19:12:03 -0400

    Added Projects Info

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -9,6 +9,8 @@ function Contacts() {
     <div>
       <p>Contacts</p>
       <br/>
+      <p>You can email me at <a href="mailto:contact@chrisryczke.com" target="_blank">contact@chrisryczke.com</a>.</p>
+      <br/>
       <a className="navButton" href="https://www.linkedin.com/in/chris-ryczke/" target="_blank">LinkedIn</a> <a className="navButton" href="https://github.com/SnowmanSixtyFour" target="_blank">GitHub</a> <a className="navButton" href={Resume} target="_blank">Resume</a> <a className="navButton" href="./socials">Social Media</a>
       <p className="separator"/>
     </div>
