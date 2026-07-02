8d522ff24d8f8053dad4fdea40b4909737ff7567
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-01 23:41:01 -0400

    Set Contacts to NavBar Properties

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -8,7 +8,7 @@ function Contacts() {
     <div>
       <p>Contacts</p>
       <br></br>
-      <a href="https://www.linkedin.com/in/chris-ryczke/">LinkedIn</a> <a href="https://github.com/SnowmanSixtyFour">GitHub</a> <a href="src\assets\ChrisRyczkeResume.pdf">Resume</a> <a href="./socials">Social Media</a>
+      <a className="navButton" href="https://www.linkedin.com/in/chris-ryczke/">LinkedIn</a> <a className="navButton" href="https://github.com/SnowmanSixtyFour">GitHub</a> <a className="navButton" href="src\assets\ChrisRyczkeResume.pdf">Resume</a> <a className="navButton" href="./socials">Social Media</a>
       <p className="separator"/>
     </div>
   )
