dc61ab44a4b8f205be9b5165013776f5707d7223
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-07-20 13:58:02 -0400

    Fixed Redirecting to Home

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function AppContent() {
         <Route path="/projects/old" element={<OlderProjects />} />
         <Route path="/socials" element={<Socials />} />
         <Route path="/contacts" element={<Contacts />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Home />} />
       </Routes>
 
       <ul className="optionsList">
